33f652628de85ae35768c15f50163247d8b6d83a
Joshua Douglas <joshua.d1434@gmail.com>
2020-02-09 12:59:20 -0800

    Starting Basic Application

---
+++ MapHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Devices.Geolocation;
+
+namespace PickaPlace
+{
+    class MapHelper
+    {
+        private MapControl fMap;
+        public MapHelper(MapControl aMap)
+        {
+            fMap = aMap;
+            fMap.MapServiceToken = "H0f4DvKp7xN8IPSykRBC~meIYzGRkcnxb14iVEVbIdQ~An-bH2h05-4L8s3QX_ImQjM0gvje-VHSBUIJoHJfrBjRLtMOQPhtfYgx8Z7U44fs";
+        }
+        public void SetInitialMapView()
+        {
+            fMap.Center = new Geopoint(new BasicGeoposition { Latitude = 34.150440, Longitude = -118.099550 });
+            fMap.ZoomLevel = 20;
+            fMap.Style = MapStyle.AerialWithRoads;
+        }
+    }
+}
